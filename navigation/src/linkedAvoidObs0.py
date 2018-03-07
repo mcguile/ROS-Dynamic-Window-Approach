a5db344f95d8a46bf75951b12c650aa101236778
Connor McGuile <mcguile@users.noreply.github.com>
2018-03-07 12:32:29 +0000

    Made Object-Oriented

--- navigation/src/linkedAvoidObs0.py
+++ navigation/src/linkedAvoidObs0.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
+# Basic node to allow avoidance of obstacle. 
+# Robot will move linearly until encountering obstacle,
+# where it will stop, turn, and move linearly again.
 # PHYSICAL BOT CODE
+# Angular velocites need to be negated in simulation
 import rospy
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
@@ -8,60 +12,63 @@ from std_msgs.msg import *
 from math import atan2
 from sensor_msgs.msg import LaserScan
 
-obstacle = False
+class Config():
+    def __init__(self):
+        # Obstacle identifier, passed to a topic
+        self.obstacle = False
+        self.speed = Twist()
+        self.r = rospy.Rate(2)
+        self.pubTele = rospy.Publisher("cmd_vel_mux/input/teleop", Twist, queue_size=1)
+        self.pubLaser = rospy.Publisher("/obstacle", Bool, queue_size=10)
+        self.subScan = rospy.Subscriber("/scan", LaserScan, config.callback)
 
-# Callback for scan
-def callback(msg):
+    # Callback for scan
+    def callback(self,msg):
 
-    minDistance = 31 # max laser range is 30
-    obsDirection = 0 # angle of obstacle from robot
-    global obstacle
+        minDistance = 31 # max laser range is 30
+        obsDirection = 0 # angle of obstacle from robot
 
-    # range of robot laser 180:540 to include a large span but not the extreme
-    # edges - 360 is directly in front of the robot
-    #PHYSICAL BOT HAS DEGREES OF 512
-    #SIMULATION HAS DEGREES OF 720
-    for angle, distance in enumerate(msg.ranges[1:510]):
-        if distance < minDistance:
-            minDistance = distance
-            obsDirection = angle
+        # PHYSICAL BOT HAS DEGREES OF 512
+        # SIMULATION HAS DEGREES OF 720
+        # Loop without extreme edge vals. They're considered later.
+        for angle, distance in enumerate(msg.ranges[1:len(msg.ranges)-2]):
+            if distance < minDistance:
+                minDistance = distance
+                obsDirection = angle
 
-    if (minDistance < 0.3):
-        obstacle = True
-        # Obstacle in front
-        # Too close: stop.
-        speed.linear.x = 0
-        # Checks if obstacle infront of robot and which direction to go
-        # enum checks between 200 -> 520, 160 is 360 between these
-        #NOTE PHYSICAL BOT HAS INVERTED LASER DEGREES RIGHT TO LEFT
-        # THEREFORE ANG VELS INVERTED
-            # Turn right
-        if (obsDirection < 255):
-            speed.angular.z = 1
-            # Turn left
+        if (minDistance < 0.3):
+            # Obstacle in front: stop.
+            self.obstacle = True
+            self.speed.linear.x = 0
+            # Check which direction to go
+            # NOTE PHYSICAL BOT HAS INVERTED LASER DEGREES RIGHT TO LEFT
+            # THEREFORE ANGULAR VELS INVERTED
+                # Turn right
+            if (obsDirection < len(msg.ranges)/2:
+                self.speed.angular.z = 1
+                # Turn left
+            else:
+                self.speed.angular.z = -1
         else:
-            speed.angular.z = -1
-    else:
-        # No obstacle in front. Move forward
-        speed.angular.z = 0
-        speed.linear.x = 0.3
-        # Checks to see if obstacle is at side
-        if (msg.ranges[0] < 0.5 or msg.ranges[511] < 0.5):
-            obstacle = True
-        else:
-            obstacle = False
+            # No obstacle in front. Move forward
+            self.speed.angular.z = 0
+            self.speed.linear.x = 0.3
+            # Checks to see if obstacle is at side
+            if (msg.ranges[0] < 0.5 or msg.ranges[len(msg.ranges)-1] < 0.5):
+                self.obstacle = True
+            else:
+                self.obstacle = False
 
-    pubLaser.publish(obstacle)
+        self.pubLaser.publish(self.obstacle)
 
-rospy.init_node("obstacles")
-pub = rospy.Publisher("cmd_vel_mux/input/teleop", Twist, queue_size=1)
-pubLaser = rospy.Publisher("/obstacle", Bool, queue_size=10)
-subScan = rospy.Subscriber("/scan", LaserScan, callback)
-
-speed = Twist()
-r = rospy.Rate(2)
-
-while not rospy.is_shutdown():
-    if(obstacle):
-        pub.publish(speed)
-    r.sleep()
+def main():
+    config = Config()
+    
+    while not rospy.is_shutdown():
+        if(config.obstacle):
+            config.pubTele.publish(config.speed)
+        config.r.sleep()
+    
+if __name__ = '__main__':
+    rospy.init_node("obstacles")
+    main()
